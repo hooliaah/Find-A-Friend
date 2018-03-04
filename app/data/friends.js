c6662d77c456f48260f6fe9b28779e273fd8e6ed
Julie Groth <groth.julie@gmail.com>
2018-03-03 22:21:30 -0800

    updated data in friends.js, added routes, required routing files in server.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,20 @@
 // save app's data inside of app/data/friends.js as an array of objects. 
 
+var friendList = [
+    {
+        name: "Julie",
+        photo: "https://goo.gl/images/jwQdhb",
+        q1: 1,
+        q2: 2,
+        q3: 3,
+        q4: 4,
+        q5: 5,
+        q6: 5,
+        q7: 5,
+        q8: 5,
+        q9: 5,
+        q10: 5
+    }
+];
+
+module.exports = friendList;
--- server.js
+++ server.js
@@ -11,6 +11,9 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
 // begin listening on server
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);

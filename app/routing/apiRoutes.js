0382d79e66ed277dcb1b5199fa90c6172837579c
Julie Groth <groth.julie@gmail.com>
2018-03-04 16:30:23 -0800

    added link to API list on home.html, updated API post route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,15 +4,16 @@
 var friendList = require('../data/friends.js');
 
 module.exports = function (app) {
-    // route to get list of all friends
+    // get route to get list of all friends
     app.get("/api/friends", function (req, res) {
         res.json(friendList);
     })
 
+    // post route to post new friend to friendList
     app.post("/api/new", function (req, res) {
         var newFriend = req.body;
-        newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-        console.log(newFriend);
+        // newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+        // console.log(newFriend);
         friendList.push(newFriend);
         res.json(newFriend);
     })

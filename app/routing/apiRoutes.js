19f961a5889261f8eaf2736e219e0a0dcf47cb33
Julie Groth <groth.julie@gmail.com>
2018-03-04 16:13:36 -0800

    added comments to survey.html and updated apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,12 @@
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
-var friendList0 = require('../data/friends.js');
+var friendList = require('../data/friends.js');
 
 module.exports = function (app) {
     // route to get list of all friends
     app.get("/api/friends", function (req, res) {
-        res.json(friendData);
+        res.json(friendList);
     })
 
     app.post("/api/new", function (req, res) {
